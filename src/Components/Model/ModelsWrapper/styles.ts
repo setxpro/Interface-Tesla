a49b7a4c3b32b73ebd05b93054dd8fba7328efc0
Patrick <patrickpqdt87289@gmail.com>
2022-04-28 10:49:57 -0300

    behavior of ScrollY

--- src/Components/Model/ModelsWrapper/styles.ts
+++ src/Components/Model/ModelsWrapper/styles.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+    height: 100vh;
+
+    scroll-snap-type: y mandatory; // Forma que funcionará o Scroll
+    overflow-y: scroll;
 `;
--- src/Components/Page/index.tsx
+++ src/Components/Page/index.tsx
@@ -7,17 +7,30 @@ import { Container } from './styles';
 const Page: React.FC = () => {
   return (
     <Container>
-      <ModelsWrapper>
+      <ModelsWrapper>   
         <div>
-            <ModelsSection
-                modelName="Model One"
+            {[
+                'Model One',
+                'Model Two',
+                'Model Three',
+                'Model Four',
+                'Model Five',
+                'Model Six',
+                'Model Seven',
+            ].map(modelName => (
+                <ModelsSection
+                key={modelName}
+                className='colored'
+                modelName={modelName}
                 overlayNode={
                     <DefaultOverlayContent
-                        label='Model One'
+                        label={modelName}
                         description="Order Online for Delivery"
                     />
                 }
             />
+            ))}
+            
         </div>
       </ModelsWrapper>
     </Container>
